272b6ec84354c3f87d21bf1eadcd7988787e57a2
Claus Wilke <wilke@austin.utexas.edu>
2013-04-22 15:14:28 -0500

    Added simple example script.

---
+++ simpleExample.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+'''
+Simple example script demonstrating how to use the PeptideBuilder library.
+
+The script generates a peptide consisting of six arginines in alpha-helix
+conformation, and it stores the peptide under the name "example.pdb".
+'''
+
+
+import Geometry
+import PeptideBuilder
+
+
+geo = Geometry.geometry('G')
+geo.phi=-60
+geo.psi_im1=-40
+structure = PeptideBuilder.initialize_res(geo)
+for i in range(5):
+	structure = PeptideBuilder.add_residue(structure, geo)
+    
+import Bio.PDB
+out = Bio.PDB.PDBIO()
+out.set_structure(structure)
+out.save( "example.pdb" )
