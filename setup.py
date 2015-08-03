bf22fc3e0d44838440f29dc76842e94ccdba8929
M Tien <Matthew.Tien89@gmail.com>
2015-08-02 22:49:52 -0500

    updating setup.py file

--- setup.py
+++ setup.py
@@ -10,12 +10,10 @@
 Setup.py script (uses setuptools) for building, testing, and installing PeptideBuilder.
 To build and install the package as root (globally), enter (from this directory!) - 
     sudo python setup.py build
-    sudo python setup.py test   # OPTIONAL BUT RECOMMENDED. Please contact author with any failed tests! Note that every once in a while the tests for functions which must generate random numbers take excessively long, so just ctrl-C these and run tests again.
     sudo python setup.py install
     
 To install for a particular user (locally), enter - 
     python setup.py build
-    python setup.py test   # OPTIONAL BUT RECOMMENDED. Please contact author with any failed tests! Note that every once in a while the tests for functions which must generate random numbers take excessively long, so just ctrl-C these and run tests again.
     python setup.py build --user # where user is the computer account to install pyolve in
 '''
 
@@ -31,5 +29,5 @@ setup(name = 'PeptideBuilder',
     download_url = 'https://github.com/smtien/PeptideBuilder/mypackage/tarball/0.0',
     platforms = 'Tested on Mac OS X.',
     packages = ['PeptideBuilder'],
-    install_requires=['numpy>=1.7', 'math', 'Bio.PDB', 'random' ]
+    install_requires=['math', 'Bio.PDB', 'random' ]
 )
